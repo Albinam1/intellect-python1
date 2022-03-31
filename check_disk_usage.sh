64bd6b93f1746d1a3dac0092a3f02a08a1d26f75
ваше_имя <albinam1709@gmail.com>
2022-03-31 16:07:15 +0000

    fixed script behaviour!

--- check_disk_usage.sh
+++ check_disk_usage.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 dir="$1"
-if [-d $dir ]; then 
-    echo -e "Checking dik usage in $dir\n"
-else echo -e "$dir directory does not exit, exiting"
+if [ -d $dir ]; then 
+    echo -e "Checking dik usage in $dir \n"
+else
+    echo -e "$dir directory does not exit, exiting"
     exit 101
 fi
-
- 
+echo  - e "The heaviest subfolder of $dir is:"
+top1=$(du -S $dir 2>/dev/null | sort -rn | head -n 1 | cut -f2)
+disk=$(du -Sh -d 0 $top1 2>/dev/null |sort -rn | head -n 1 | cut -f1)
+echo -e "$top1 with disk usage of $disk"
+#echo "The heavist subfolder of $dir is $top1" | festival -tts
